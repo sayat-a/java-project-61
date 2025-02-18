39751492bceda8d484b47fde495a73cb05bbff02
Sayat Aubakirov <sayat_aubakirov@mail.ru>
2025-02-19 00:04:53 +0500

    update src/main/java/hexlet/code/App.java

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,26 @@
 package hexlet.code;
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Cli.greetUser();
+        var scanner = new Scanner(System.in);
+
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+
+        var choice = scanner.nextLine();
+        switch (choice) {
+            case "1":
+                Cli.greetUser();
+                break;
+            case "2":
+                BrainEven.start();
+                break;
+            case "0":
+                break;
+        }
     }
 }
